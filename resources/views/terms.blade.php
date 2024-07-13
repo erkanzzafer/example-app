01d15ef64c35ff97a893be4da4b20f7694264566
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-07-13 16:14:18 +0300

    layout completed

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.layout')
+@section('content')
+<h1>TERMS</h1>
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur optio earum quisquam velit
+    quod neque dolor voluptate! Tempore dolores reprehenderit iure accusantium ea recusandae modi natus facere
+    vero aliquid.
+    Rem, esse necessitatibus. Unde quasi nihil delectus nam doloribus, aliquam similique voluptates dicta
+    repudiandae doloremque laudantium libero, ipsum sed facere, veritatis eum possimus rerum totam quibusdam.
+    Distinctio doloremque inventore natus.
+    Neque possimus quaerat alias expedita hic aperiam ab nesciunt repudiandae, repellat, doloribus iste aut!
+    Culpa reprehenderit, reiciendis vero voluptatibus sequi, officia fuga veritatis, possimus omnis nobis
+    asperiores facere perferendis eligendi!
+    Tempore praesentium officiis quas beatae? Debitis voluptatum est voluptates quas iusto voluptatibus ut
+    possimus itaque magnam veniam neque dolorum libero perspiciatis expedita vero sit, explicabo nihil eaque
+    asperiores atque veritatis!
+    A maxime, voluptatem quasi magni officiis temporibus impedit, sunt, quo autem aspernatur corporis saepe eum?
+    Iure obcaecati rem doloremque repellat nostrum quae, placeat, fugit perspiciatis delectus error nobis
+    reprehenderit commodi.
+</div>
+
+@endsection
