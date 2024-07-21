03269f7e33505a3012294b1f99d23bdef6dfc5ac
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-07-21 17:41:04 +0300

    fix pagination

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,28 @@
 @extends('layout.layout')
 @section('content')
-<div class="row">
-    <div class="col-3">
-        @include('shared.left-sidebar')
-    </div>
-    <div class="col-6">
-        @include('shared.success-message')
-        @include('shared.submit-idea')
-        <hr>
-        @foreach ($ideas as $idea)
-            <div class="mt-3">
-                @include('shared.idea-card')
-            </div>
-        @endforeach
-        {{$ideas->links()}}
-    </div>
-    <div class="col-3">
-        @include('shared.search-box')
-        @include('shared.follow-box')
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            @include('shared.success-message')
+            @include('shared.submit-idea')
+            <hr>
+            @if (count($ideas) > 0)
+                @foreach ($ideas as $idea)
+                    <div class="mt-3">
+                        @include('shared.idea-card')
+                    </div>
+                @endforeach
+            @else
+                No result Found
+            @endif
+            {{ $ideas->withQueryString()->links() }}
+        </div>
+        <div class="col-3">
+            @include('shared.search-box')
+            @include('shared.follow-box')
 
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/shared/search-box.blade.php
+++ resources/views/shared/search-box.blade.php
@@ -3,8 +3,9 @@
         <h5 class="">Search</h5>
     </div>
     <div class="card-body">
-        <form action="{{route('dashboard')}}" method="GET">
-            <input placeholder="..." class="form-control w-100" type="text" name="search">
+        <form action="{{ route('dashboard') }}" method="GET">
+            <input value="{{ request('search', '') }}" placeholder="..." class="form-control w-100" type="text"
+                name="search">
             <button class="btn btn-dark mt-2"> Search</button>
         </form>
     </div>
